15dba21576f24298eab519cae944f1ce642b0e06
Kilho Chang <khjchang@ucdavis.edu>
2026-06-20 23:08:56 -0700

    update email

---
+++ js/email-links.js
@@ -0,0 +1,17 @@
+document.querySelectorAll(".social.email").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const user = link.dataset.emailUser;
+    const domain = link.dataset.emailDomain;
+    const tld = link.dataset.emailTld;
+
+    if (!user || !domain || !tld) {
+      return;
+    }
+
+    const email = `${user}@${domain}.${tld}`;
+    const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(email)}`;
+    window.open(url, "_blank", "noopener");
+  });
+});
